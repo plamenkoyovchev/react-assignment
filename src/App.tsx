bc4a79788b543eeab00c6abde2d4eccfee424bb7
Plamen Yovchev <plamenkooo@gmail.com>
2020-09-09 01:18:32 +0300

    Move Route logic into AppRouter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import React, { useEffect } from "react";
 import "./App.css";
 
+import AppRouter from "./components/Router/AppRouter";
+
 import { useSelector, useDispatch } from "react-redux";
 import { RootStore } from "./store/store";
 
 import { setCurrentUser } from "./store/users/userActions";
-
-import HomePage from "./Pages/HomePage";
-import { Switch, Route, Redirect } from "react-router-dom";
-import PrivateRoute from "./components/Navigation/PrivateRoute";
-
-import LoginForm from "./components/LoginForm";
-
-import ItemsPage from "./Pages/ItemsPage";
-import ProgressPage from "./Pages/ProgressPage";
-import ProfilePage from "./Pages/ProfilePage";
-import AdministrationPage from "./Pages/AdministrationPage";
-
-import Navigation from "./components/Navigation/Navigation";
-
-import { UserRoles } from "./models/UserRoles";
 
 function App() {
   const { currentUser } = useSelector((state: RootStore) => state.user);
@@ -31,53 +18,7 @@ function App() {
 
   return (
     <div className="App">
-      <Route exact path="/" component={HomePage} />
-      <Route
-        path={"/(.+)"}
-        render={() => (
-          <>
-            <Navigation />
-            <Switch>
-              <Route
-                path="/auth"
-                render={() =>
-                  currentUser ? <Redirect to="/" /> : <LoginForm />
-                }
-              />
-              <PrivateRoute
-                path="/items"
-                Component={ItemsPage}
-                userRole={currentUser?.role}
-                requiredRoles={[
-                  String(UserRoles.Admin),
-                  String(UserRoles.User),
-                ]}
-              />
-              <PrivateRoute
-                path="/progress"
-                Component={ProgressPage}
-                userRole={currentUser?.role}
-                requiredRoles={[String(UserRoles.User)]}
-              />
-              <PrivateRoute
-                path="/profile"
-                Component={ProfilePage}
-                userRole={currentUser?.role}
-                requiredRoles={[
-                  String(UserRoles.Admin),
-                  String(UserRoles.User),
-                ]}
-              />
-              <PrivateRoute
-                path="/administration"
-                Component={AdministrationPage}
-                userRole={currentUser?.role}
-                requiredRoles={[String(UserRoles.Admin)]}
-              />
-            </Switch>
-          </>
-        )}
-      />
+      <AppRouter currentUser={currentUser} />
     </div>
   );
 }
---
+++ src/components/Router/AppRouter.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+import HomePage from "../../Pages/HomePage";
+import { Switch, Route, Redirect } from "react-router-dom";
+import PrivateRoute from "../Navigation/PrivateRoute";
+
+import LoginForm from "../LoginForm";
+
+import ItemsPage from "../../Pages/ItemsPage";
+import ProgressPage from "../../Pages/ProgressPage";
+import ProfilePage from "../../Pages/ProfilePage";
+import AdministrationPage from "../../Pages/AdministrationPage";
+
+import Navigation from "../Navigation/Navigation";
+
+import { UserRoles } from "../../models/UserRoles";
+import { User } from "../../store/users/userActionTypes";
+
+interface IProps {
+  currentUser?: User;
+}
+
+const AppRouter: React.FC<IProps> = ({ currentUser }) => {
+  return (
+    <>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <>
+            <Navigation />
+            <Switch>
+              <Route
+                path="/auth"
+                render={() =>
+                  currentUser ? <Redirect to="/" /> : <LoginForm />
+                }
+              />
+              <PrivateRoute
+                path="/items"
+                Component={ItemsPage}
+                userRole={currentUser?.role}
+                requiredRoles={[
+                  String(UserRoles.Admin),
+                  String(UserRoles.User),
+                ]}
+              />
+              <PrivateRoute
+                path="/progress"
+                Component={ProgressPage}
+                userRole={currentUser?.role}
+                requiredRoles={[String(UserRoles.User)]}
+              />
+              <PrivateRoute
+                path="/profile"
+                Component={ProfilePage}
+                userRole={currentUser?.role}
+                requiredRoles={[
+                  String(UserRoles.Admin),
+                  String(UserRoles.User),
+                ]}
+              />
+              <PrivateRoute
+                path="/administration"
+                Component={AdministrationPage}
+                userRole={currentUser?.role}
+                requiredRoles={[String(UserRoles.Admin)]}
+              />
+            </Switch>
+          </>
+        )}
+      />
+    </>
+  );
+};
+
+export default AppRouter;
