98989e8fb7015353bdd641e08f32c890a6e9491c
Plamen Yovchev <plamenkooo@gmail.com>
2020-09-08 20:26:07 +0300

    Use Loader component in HomePage

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -5,8 +5,14 @@ import LoginForm from "../components/LoginForm";
 import { useSelector } from "react-redux";
 import { RootStore } from "../store/store";
 
+import Loader from "../components/UI/Loader";
+
 const HomePage = () => {
-  const { loggedIn } = useSelector((state: RootStore) => state.user);
+  const { loggedIn, loading } = useSelector((state: RootStore) => state.user);
+
+  if (loading) {
+    return <Loader />;
+  }
 
   return <>{loggedIn ? <Navigation /> : <LoginForm />}</>;
 };
