abaccd5d7f3cf6a38a8c5de3a509538f0906dbb4
Plamen Yovchev <plamenkooo@gmail.com>
2020-09-08 20:26:28 +0300

    Add navigation links and styles

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,7 +3,29 @@ import React from "react";
 import "./Navigation.css";
 
 const Navigation = () => {
-  return <h1>Welcome</h1>;
+  return (
+    <header className="main-header">
+      <nav className="main-nav">
+        <ul className="main-nav__items">
+          <li className="main-nav__item">
+            <a href="#">Items</a>
+          </li>
+          <li className="main-nav__item">
+            <a href="#">Progress</a>
+          </li>
+          <li className="main-nav__item">
+            <a href="#">Profile</a>
+          </li>
+          <li className="main-nav__item">
+            <a href="#">Administration</a>
+          </li>
+          <li className="main-nav__item main-nav__item--cta">
+            <a href="#">Logout</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Navigation;
